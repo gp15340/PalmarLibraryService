ade42db4ebac400e9a7b8ce37105d892cbba1dcf
guopeng <627829348@qq.com>
2018-05-24 10:33:18 +0800

    5/24 User bookType

---
+++ src/com/palmarLibrary/bean/BookType.java
@@ -0,0 +1,33 @@
+package com.palmarLibrary.bean;
+
+
+import java.util.HashSet;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="type")
+public class BookType {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer typeId;
+	private String typeName;
+	public Integer getTypeId() {
+		return typeId;
+	}
+	public void setTypeId(Integer typeId) {
+		this.typeId = typeId;
+	}
+	public String getTypeName() {
+		return typeName;
+	}
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+	
+	
+}
